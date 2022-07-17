cf69ba6254e8cf64b59ab8cd1e726e9a0a0b0c8c
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-07-17 01:44:59 -0700

    Added the padding to IconCategory

--- components/IconCategory.jsx
+++ components/IconCategory.jsx
@@ -14,7 +14,7 @@ import "swiper/css";
 
 const IconCategory = () => {
   return (
-    <div className="w-full mt-10">
+    <div className="w-full mt-10 lg:px-10">
       <Swiper
         direction="horizontal"
         slidesPerView={2}
