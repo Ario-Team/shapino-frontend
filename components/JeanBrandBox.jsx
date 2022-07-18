ca26404db2e96442e8ed57688b454cb65b3ac286
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-07-18 00:20:47 -0700

    Converted the box to standalone component

---
+++ components/JeanBrandBox.jsx
@@ -0,0 +1,26 @@
+import { RiArrowLeftSLine } from "react-icons/ri";
+import BrandsBox from "./BrandsBox";
+
+const JeanBrandBox = () => {
+  return (
+    <BrandsBox
+      className="w-11/12 flex flex-col h-48 items-center bg-left justify-center gap-5 pl-14 lg:bg-center lg:w-6/12 lg:items-end lg:h-[200px]"
+      backgroundImage={'url("/images/boxBackground.png")'}
+    >
+      <span className="font-kalameh font-bold text-2xl">شلوار جین وست</span>
+      <span className="font-kalameh font-bold text-3xl text-[#FF6230]">
+        jean
+      </span>
+      <div className="flex flex-row w-max cursor-pointer items-center transition-all duration-300 hover:shadow-md">
+        <span className="font-iran-yekan font-medium text-sm bg-white px-3 py-1 rounded-r-md md:text-base">
+          دیدن همه
+        </span>
+        <div className="bg-black p-1.5 rounded-l-md md:p-2">
+          <RiArrowLeftSLine className="text-white" />
+        </div>
+      </div>
+    </BrandsBox>
+  );
+};
+
+export default JeanBrandBox;
--- pages/index.jsx
+++ pages/index.jsx
@@ -4,11 +4,8 @@ import Head from "next/head";
 import Menu from "../components/Menu";
 import IconCategory from "../components/IconCategory";
 import OfferedProducts from "../components/OfferedProducts";
-import BrandsBox from "../components/BrandsBox";
-import { RiArrowLeftSLine } from "react-icons/ri";
-import Image from "next/image";
-import backgroundImage from "../public/images/boxBackground.png";
 import SonyBox from "../components/SonyBox";
+import JeanBrandBox from "../components/JeanBrandBox";
 
 export default function Home() {
   return (
@@ -24,25 +21,7 @@ export default function Home() {
           <OfferedProducts />
           <div className="flex flex-col items-center gap-5 w-full justify-between my-10 lg:flex-row">
             <SonyBox />
-            <BrandsBox
-              className="w-11/12 flex flex-col h-48 items-center bg-left justify-center gap-5 pl-14 lg:bg-center lg:w-6/12 lg:items-end lg:h-[200px]"
-              backgroundImage={'url("/images/boxBackground.png")'}
-            >
-              <span className="font-kalameh font-bold text-2xl">
-                شلوار جین وست
-              </span>
-              <span className="font-kalameh font-bold text-3xl text-[#FF6230]">
-                jean
-              </span>
-              <div className="flex flex-row items-center font-iran-yekan font-medium text-sm">
-                <span className="bg-white px-3 py-2 rounded-r-md">
-                  مشاهده همه
-                </span>
-                <div className="bg-black px-2 py-1.5 rounded-l-md">
-                  <RiArrowLeftSLine className="text-2xl text-white" />
-                </div>
-              </div>
-            </BrandsBox>
+            <JeanBrandBox />
           </div>
         </div>
       </MainLayout>
