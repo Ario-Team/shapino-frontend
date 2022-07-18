73c465c435c124dbf74f97d22a87e8ebd79f96bd
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-07-18 01:41:16 -0700

    Added eager loader

--- components/PopularBrandsBox.jsx
+++ components/PopularBrandsBox.jsx
@@ -6,7 +6,13 @@ const PopularBrandsBox = ({ src, count, rate }) => {
   return (
     <div className="group cursor-pointer bg-white w-max px-7 py-5 rounded-lg transition-all duration-300 drop-shadow-md hover:drop-shadow-xl hover:scale-105 hover:shadow-[#FF6230]/40 hover:shadow-md">
       <div>
-        <Image src={src} width={160} height={160} alt="Brands icon" />
+        <Image
+          loading="eager"
+          src={src}
+          width={160}
+          height={160}
+          alt="Brands icon"
+        />
       </div>
       <div className="flex flex-col items-center font-iran-yekan opacity-0 transition-all duration-300 group-hover:opacity-100 ">
         <span className="font-bold text-xl">{count}+ کالا</span>
