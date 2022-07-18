4788d0041711e267019c801e4691c90914eb9fe9
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-07-18 00:33:44 -0700

    Added the px-3

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -54,7 +54,7 @@ const Navbar = () => {
             placeholder="جستجو بیش از 8 هزار کالا"
             className="font-iran-yekan font-normal bg-transparent outline-none border-2 text-sm pr-2 border-[#7C8DA6]/30 rounded-lg w-full h-14 transition-colors duration-300 hover:border-[#7C8DA6]/50 focus:border-[#7C8DA6]/50"
           />
-          <div className=" flex justify-center items-center w-max h-max absolute top-3 left-3 bg-gray-400 bg-opacity-80 px-2 py-1.5 rounded-lg transition-colors duration-300 cursor-pointer hover:bg-gray-500">
+          <div className=" flex justify-center items-center w-max h-max absolute top-3 left-3 bg-gray-400 bg-opacity-80 px-3 py-1.5 rounded-lg transition-colors duration-300 cursor-pointer hover:bg-gray-500">
             <Image src={searchSVG} width={20} height={20} alt="search icon" />
           </div>
         </div>
