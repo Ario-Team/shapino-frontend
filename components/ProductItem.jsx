1b444b9e327b6deb96174bc4a790a2f20e76333b
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-07-22 01:26:36 -0700

    Add link to productItems

--- components/ProductItem.jsx
+++ components/ProductItem.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { FaStar } from "react-icons/fa";
 import shoesSVG from "../public/svgs/shoes.svg";
 
@@ -11,33 +12,35 @@ const ProductItem = ({
   originalPrice,
 }) => {
   return (
-    <div className="relative w-max cursor-pointer bg-white px-9 flex flex-col items-center transition-all duration-300 hover:scale-105 hover:shadow-[#FF6230]/30 hover:shadow-md ">
-      <div className="absolute left-6 top-5 flex flex-row gap-2 items-center">
-        <FaStar className="text-yellow-400" />
-        {rate}
-      </div>
-      <Image src={shoesSVG} width={150} height={150} alt="Product image" />
-      <span className="w-44 text-center font-iran-yekan font-medium text-lg">
-        {title}
-      </span>
-      {offer ? (
-        <div className="flex flex-row items-center gap-8  mt-10">
-          <span className="text-white bg-red-600 font-iran-yekan font-medium text-base flex items-center justify-center py-0.5 px-3 rounded-sm">
-            {offerPercent}
-          </span>
-
-          <span className="text-red-600 line-through">
-            <span className="text-black font-iran-yekan font-medium text-base">
-              {originalPrice}
-            </span>
-          </span>
+    <Link href="/product" passHref prefetch>
+      <div className="relative w-max cursor-pointer bg-white px-9 flex flex-col items-center transition-all duration-300 hover:scale-105 hover:shadow-[#FF6230]/30 hover:shadow-md ">
+        <div className="absolute left-6 top-5 flex flex-row gap-2 items-center">
+          <FaStar className="text-yellow-400" />
+          {rate}
         </div>
-      ) : null}
-      <span className="flex flex-row items-center gap-2 mt-5 mb-10 text-black font-iran-yekan font-bold text-xl">
-        {offerPrice}
-        <span className="font-iran-yekan font-medium text-base">تومان</span>
-      </span>
-    </div>
+        <Image src={shoesSVG} width={150} height={150} alt="Product image" />
+        <span className="w-44 text-center font-iran-yekan font-medium text-lg">
+          {title}
+        </span>
+        {offer ? (
+          <div className="flex flex-row items-center gap-8  mt-10">
+            <span className="text-white bg-red-600 font-iran-yekan font-medium text-base flex items-center justify-center py-0.5 px-3 rounded-sm">
+              {offerPercent}
+            </span>
+
+            <span className="text-red-600 line-through">
+              <span className="text-black font-iran-yekan font-medium text-base">
+                {originalPrice}
+              </span>
+            </span>
+          </div>
+        ) : null}
+        <span className="flex flex-row items-center gap-2 mt-5 mb-10 text-black font-iran-yekan font-bold text-xl">
+          {offerPrice}
+          <span className="font-iran-yekan font-medium text-base">تومان</span>
+        </span>
+      </div>
+    </Link>
   );
 };
 
