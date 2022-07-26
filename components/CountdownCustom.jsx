0cb3f386417d12166d030a0b34f37e75687ddab3
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2022-07-26 03:11:10 -0700

    Fixed the Countdown

--- components/CountdownCustom.jsx
+++ components/CountdownCustom.jsx
@@ -8,7 +8,18 @@ const CountdownCustom = () => {
     <div className="flex flex-col w-11/12 mx-auto h-[22rem] justify-between relative bg-[#F2F0F1] px-7 py-10 sm:w-max">
       <div className="relative flex self-center">
         <div className="flex items-center gap-2">
-          <div className="w-[5.5rem] h-5 bg-orange-500"></div>
+          <div className="bg-orange-500 px-3">
+            <Countdown
+              date={Date.now() + 10000000}
+              renderer={({ hours, minutes, seconds }) => {
+                return (
+                  <div className="font-kalameh font-bold text-2xl text-black">
+                    {hours}:{minutes}:{seconds}
+                  </div>
+                );
+              }}
+            />
+          </div>
           <div>
             <Image
               src={counterClockSVG}
@@ -18,20 +29,8 @@ const CountdownCustom = () => {
             />
           </div>
         </div>
-        <div className="absolute -right-5">
-          <Countdown
-            date={Date.now() + 10000000}
-            renderer={({ hours, minutes, seconds }) => {
-              return (
-                <div className="font-kalameh font-bold text-3xl text-black">
-                  {hours}:{minutes}:{seconds}
-                </div>
-              );
-            }}
-          />
-        </div>
       </div>
-      <span className="self-center lg:absolute w-max top-28 -right-3.5 font-kalameh font-bold text-3xl text-[#FF6230] my-10">
+      <span className="self-center w-max top-28 -right-3.5 font-kalameh font-bold text-3xl text-[#FF6230] my-10">
         تخفیفات ویژه
       </span>
       <div className="w-max flex flex-row opacity-90 self-center transition-opacity duration-300 cursor-pointer shadow-sm hover:opacity-100">
